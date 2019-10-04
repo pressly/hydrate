a5c7fd39ca1fd2555734d28fb9013bbfee631bbb
Vojtech Vitek <vojtech.vitek@visioncritical.com>
2019-10-04 17:07:09 +0200

    Update README

--- README.md
+++ README.md
@@ -1,7 +1,45 @@
 # Hydrate secrets from AWS Systems Manager Parameter Store
 
-## Usage:
-```
+Hydrate JSON, YAML, TOML config files.
 
-hydrate input.yml > ouput.yml
-```
+Replace all matching string values with strings/secrets from AWS SSM Param Store.
+1. `"$SECRET:/custom/parameter/path"`
+2. `"$$"`
+3. `"$SECRET"`
+
+## Usage:
+### Hydrate JSON file:
+    hydrate no-secrets.json > secrets.json
+
+### Hydrate YAML data from stdin:
+    echo "data: $SECRET:/app/sit1/app_secret_data_key" | hydrate --format=yml - > secret.yml
+
+### Hydrate Kubernetes Secrets/ConfigMap data files/values
+Hydrate both "data" and "stringData" fields (handles base64 encoding automatically).
+
+    hydrate -k8s k8s-secret.yml | kubectl apply -
+
+## Example:
+
+### Parameter Store:
+    PATH                     VALUE
+    /custom/parameter/path   a
+    /prefix/db_passwd        bb
+    /prefix/db_pwd           ccc
+    
+### input.json:
+    {
+        "db_password": "$SECRET:/custom/parameter/path",
+        "db_passwd": "$$",
+        "db_pwd": "$SECRET",
+    }
+	
+### Run command:
+    hydrate --path=/prefix input.json > output.json
+
+### output.json:
+    {
+        "db_password": "a",
+        "db_passwd": "bb",
+        "db_pwd": "ccc",
+    }
--- cmd/hydrate/main.go
+++ cmd/hydrate/main.go
@@ -25,17 +25,48 @@ var (
 
 	usage = errors.New(`hydrate:
 
-Hydrate values matching "^\$SECRET:" regex with values from AWS SSM Param Store.
+Hydrate	JSON, YAML, TOML config files.
 
-usage:
-	# hydrate JSON file
-		hydrate no-secrets.json > secrets.json
+Replace all matching values with strings/secrets from AWS SSM Param Store.
+    1. "$SECRET:/custom/parameter/path"
+    2. "$$"
+    3. "$SECRET"
 
-	# hydrate YAML data from stdin
-		echo "data: $SECRET:/app/sit1/app_secret_data_key" | hydrate --format=yml - > secret.yml
+Usage:
+    # Hydrate JSON file:
+        hydrate no-secrets.json > secrets.json
 
-	# hydrate Kubernetes Secrets/ConfigMap "stringData" and base64-encoded "data" files/values
-		hydrate -k8s k8s-secret.yml | kubectl apply -
+    # Hydrate YAML data from stdin:
+        echo "data: $SECRET:/app/sit1/app_secret_data_key" | hydrate --format=yml - > secret.yml
+
+	# Hydrate Kubernetes Secrets/ConfigMap data files/values
+	# (both "data" and "stringData" fields, handles base64 encoding automatically):
+        hydrate -k8s k8s-secret.yml | kubectl apply -
+
+Example:
+
+    Parameter Store:
+        PATH                     VALUE
+        /custom/parameter/path   a
+        /prefix/db_passwd        bb
+        /prefix/db_pwd           ccc
+    
+	input.json:
+		{
+			"db_password": "$SECRET:/custom/parameter/path",
+			"db_passwd": "$$",
+			"db_pwd": "$SECRET",
+		}
+	
+	Run command:
+        hydrate --path=/prefix input.json > output.json
+
+    output.json:
+		{
+			"db_password": "a",
+			"db_passwd": "bb",
+			"db_pwd": "ccc",
+		}
 `)
 )
 
